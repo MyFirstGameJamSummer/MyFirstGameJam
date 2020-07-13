85d7ab523dc8807c98c237761a753bd4b777e52f
peyvand-pournasir <peyvandpournasir.pp@gmail.com>
2020-07-13 13:21:15 +0430

    Hero Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
    private float horizontalInput;
    private float verticalInput;
    private Vector2 playerVelocity;
+    public Animator animator;
+    public GameObject Hero;
    
 
     // Start is called before the first frame update
@@ -20,14 +22,17 @@ public class PlayerMovement : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         playerVelocity = Vector2.zero;
+        animator.GetComponent<Animator>();
     }
 
 
-    
+
+
     void FixedUpdate()
     {
         playerVelocity = Vector2.zero;
-        
+        isWalking = false;
+
         
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -35,13 +40,47 @@ public class PlayerMovement : MonoBehaviour
         if (horizontalInput != 0)
         {
             playerVelocity.x = horizontalInput * speed;
+           
             isWalking = true;
+            
+            
+
+            
+            
         }
+        
 
         if (verticalInput != 0)
         {
             playerVelocity.y = verticalInput * speed;
+            isWalking = true;
+            
+
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("iswalking", true);
+            animator.SetBool("isIdle", false);
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("iswalking", true);
+            animator.SetBool("isIdle", false);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("iswalking", true);
+            animator.SetBool("isIdle", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("iswalking", true);
+            animator.SetBool("isIdle", false);
+        } else
+        {
+            animator.SetBool("iswalking", false);
+            animator.SetBool("isIdle", true);
+        }
+
 
         rb2d.velocity = playerVelocity;
 
