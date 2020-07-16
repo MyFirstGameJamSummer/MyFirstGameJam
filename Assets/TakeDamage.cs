e6c95abd3d62cd4be4eb19b7e2e12826797d4d47
peyvand-pournasir <peyvandpournasir.pp@gmail.com>
2020-07-16 16:24:39 +0430

    healthbar

---
+++ Assets/TakeDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthbar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamages(20);
+        }
+    }
+
+    public void TakeDamages (int Damage)
+    {
+        currentHealth -= Damage;
+
+        healthbar.SetHealth(currentHealth);
+    }
+    
+}
