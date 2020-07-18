b778b37c24f035ae73d1e7fc7315c778e02020b8
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2020-07-18 18:54:16 +0430

    MeleeAttack

---
+++ Assets/Scripts/Enemy/EnemyRed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRed : Enemy
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void EnemyAnimatorAttacking()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void EnemyAnimatorDying()
+    {
+        throw new System.NotImplementedException();
+    }
+}
