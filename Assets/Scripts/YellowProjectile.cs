3e66fe5fa6e5415d94aa019d85e6d8656059767f
peyvand-pournasir <peyvandpournasir.pp@gmail.com>
2020-07-19 16:47:12 +0430

    tt

--- Assets/Scripts/YellowProjectile.cs
+++ Assets/Scripts/YellowProjectile.cs
@@ -9,21 +9,36 @@ public class YellowProjectile : MonoBehaviour
     public AIDestinationSetter AIDes;
      GameObject hero;
 
+    private Vector3 initpos;
+    [SerializeField] private float destroyDistance;
+    Animator anim;
 
     private void Awake()
     {
         hero = GameObject.FindGameObjectWithTag("Player");
         AIDes.target = hero.transform;
+       
     }
 
     void Start()
     {
-        
+        initpos = transform.position;
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(initpos, transform.position) > destroyDistance)
+        {
+            anim.SetTrigger("Destroy");
+            
+
+        }
+    }
+    private void DestroyProjectile()
+    {
+        Destroy(gameObject,0.3f );
     }
 }
