32f12038e9469f255e5f5d27b88af6fe4ea97c49
peyvand-pournasir <peyvandpournasir.pp@gmail.com>
2020-07-19 11:24:01 +0430

    /

---
+++ Assets/Scripts/YellowProjectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class YellowProjectile : MonoBehaviour
+{
+    public AIDestinationSetter AIdes;
+    public GameObject hero;
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
