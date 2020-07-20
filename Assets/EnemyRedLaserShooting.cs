0813ad6812d9dc6f4f50d55f8828e5c788cda294
peyvand-pournasir <peyvandpournasir.pp@gmail.com>
2020-07-20 18:11:49 +0430

    ..

---
+++ Assets/EnemyRedLaserShooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRedLaserShooting : MonoBehaviour
+{
+    public Animator animRed;
+    public GameObject BigLaser;
+    public GameObject SmallLaser;
+   
+
+   public void SetBigLaserActive()
+    {
+        BigLaser.SetActive(true);
+        SmallLaser.SetActive(false);
+    }
+    public void SetSmallLaserActive()
+    {
+        SmallLaser.SetActive(true);
+        BigLaser.SetActive(false);
+    }
+    public void FinishAttack()
+    {
+        SmallLaser.SetActive(false);
+        BigLaser.SetActive(false);
+    }
+    public void animReset()
+    {
+        animRed.SetBool("IsAttacking", false);
+        animRed.SetBool("IsIdle", true);
+    }
+    
+
+}
